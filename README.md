d9b73c811bbcf3849b979658477894f5db41b38a
manoharprabhu <manoharagent13@gmail.com>
2016-12-12 16:15:52 +0530

    Add travis badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/manoharprabhu/WellRested.svg?branch=master)](https://travis-ci.org/manoharprabhu/WellRested)
+
 A Spring boot application to expose a Relation database as a REST service. Currently supports MYSQL.
 
 Work in progress, documentation in progress.
--- src/main/java/com/manoharprabhu/wellrested/service/RowsLimitBuilder.java
+++ src/main/java/com/manoharprabhu/wellrested/service/RowsLimitBuilder.java
@@ -7,6 +7,8 @@ import org.json.JSONObject;
  * Created by mprabhu on 12/12/2016.
  */
 public class RowsLimitBuilder {
+    public static final String FROM_ROW = "fromRow";
+    public static final String COUNT = "count";
     private JSONObject limits;
     private DatabaseType databaseType;
 
@@ -16,14 +18,13 @@ public class RowsLimitBuilder {
     }
 
     public String build() {
-
-        if(limits.keySet().contains("fromRow") == false
-                || limits.keySet().contains("count") == false) {
+        if(limits.keySet().contains(FROM_ROW) == false
+                || limits.keySet().contains(COUNT) == false) {
             return "";
         }
 
         if(this.databaseType == DatabaseType.MYSQL) {
-            return " LIMIT " + limits.getString("fromRow") + " ," + limits.getString("count");
+            return " LIMIT " + limits.getInt(FROM_ROW) + " ," + limits.getInt(COUNT);
         } else {
             return "";
         }
